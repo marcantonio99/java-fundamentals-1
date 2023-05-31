73e44944f2dec08edaf3a96c7bb685d1574c3449
marcantonio99 <marcantonio8799@gmail.com>
2023-05-31 14:50:57 +0200

    first exercise finished

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,4 +1,33 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class TrainTicket {
+    public static void main(String[] args) {
+
+        //declare the variables
+        int km;
+        int age;
+        double ticketPriceTot;
+
+        //scanner
+        Scanner keyBoardReader = new Scanner(System.in);
+
+        System.out.println("inserisci i Km da percorrere: ");
+        km = keyBoardReader.nextInt();
+        System.out.println("inserisci la tua età: ");
+        age = keyBoardReader.nextInt();
+
+        double ticketPrice = (km * 0.21);
+
+        if (age < 18){
+            ticketPriceTot = ticketPrice - (ticketPrice*0.20);
+            System.out.println("Il prezzo del tuo biglietto è di: " + ticketPriceTot + "euro");
+        } else if (age > 65) {
+            ticketPriceTot = ticketPrice - (ticketPrice*0.40);
+            System.out.println("Il prezzo del tuo biglietto è di: " + ticketPriceTot + "euro");
+        }else{
+            System.out.println("Il prezzo del tuo biglietto è di: " + ticketPrice + "euro");
+        }
+    }
 }
